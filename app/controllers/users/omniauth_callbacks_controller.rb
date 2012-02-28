bea3f967e49e11b7fb53abb74bd8ce096426e6b1
Michal Cichra <michal@o2h.cz>
2012-02-28 12:38:02 +0100

    added omniauth callback controller

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,10 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
+  def twitter
+    raise "TWITTER CALLBACK"
+  end
+
+  def facebook
+    raise "FACEBOOK CALLBACK"
+  end
+end
