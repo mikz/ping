e9a6c1c9a8449dbb54cd91963e8853c046ab4944
Michal Cichra <michal@o2h.cz>
2012-02-28 17:06:50 +0100

    create location aware pong

--- app/controllers/pongs_controller.rb
+++ app/controllers/pongs_controller.rb
@@ -1,10 +1,19 @@
 class PongsController < ApplicationController
+  respond_to :json
 
   def index
-    render json: Pong.all.to_json
+    @pongs = Pong.limit(100)
+
+    respond_with(@pongs)
   end
 
   def create
-    @pong = current_user.pongs.create 
+    attrs = {}
+    if coords = params[:coords]
+      attrs.merge! coords.slice(:latitude, :longitude)
+    end
+    @pong = current_user.pongs.create attrs
+
+    respond_with(@pong)
   end	
 end
