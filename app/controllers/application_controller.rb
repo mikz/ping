e09bac6c75e9237fbaaf287f02a583b51995e7ff
Michal Cichra <michal@o2h.cz>
2012-02-28 15:08:13 +0100

    friends controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+
+  private
+  def require_login
+    redirect_to(root_path) unless signed_in?
+    false
+  end
 end
--- app/controllers/import/friends_controller.rb
+++ app/controllers/import/friends_controller.rb
@@ -1,2 +1,12 @@
 class Import::FriendsController < ApplicationController
+  before_filter :require_login
+
+  respond_to :xml, :json
+
+  def create
+    @friends = current_user.import_friends!
+
+    respond_with(@friends)
+  end
+
 end
